bc8556dcc0185a961ed02685e9d62db963f1fc8a
Subhratanu <subhratanu.jisce@gmail.com>
2024-03-29 20:25:55 +0530

    Middleware commit

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 8080;
-
-mongodb.mongoDBConnection();
+app.use(express.urlencoded({ extended: false }));
+// mongodb.mongoDBConnection();
 
 app.use("/api", userRouter);
 // app.use("/api/employee", employeeRouter);
 
+app.post("/samplepost", (req, res, next) => {
+  const name = req.body.name;
+  return res.send(name);
+});
+app.use((req, res, next) => {
+  if (false) {
+    next();
+  } else {
+    return res.send("Back to Post 1");
+  }
+});
+app.use((req, res, next) => {
+  if (true) {
+    next();
+  } else {
+    return res.send("Back to Post 2");
+  }
+});
+app.get("/sampleget", (req, res) => {
+  return res.send("It's Finally here!");
+});
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
